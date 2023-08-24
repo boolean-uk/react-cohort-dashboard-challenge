d64918560f89d70ff8741a08d27db5c926c7f983
iliask796 <iliaskal7@gmail.com>
2023-08-24 13:44:43 +0300

    App Grid Area Styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './app.css';
 function App() {
   return (
     <div className="app">
-      
+      <div className='header'>
+
+      </div>
+      <div className='nav'>
+
+      </div>
+      <div className='main'>
+
+      </div>
     </div>
   );
 }
