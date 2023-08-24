19cc3064a342c45662150323190d84eab73d23c3
ks46 <konastaf46@gmail.com>
2023-08-24 22:41:59 +0300

    created Header component
    * removed redundant code and files generated from create-react-app
    * header logo

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import titleHeader from '../assets/title-header.svg'
+
+export default function Header() {
+  
+  return (
+    <header>
+      <img src={titleHeader} alt='title header'/>
+    </header>
+  )
+}
