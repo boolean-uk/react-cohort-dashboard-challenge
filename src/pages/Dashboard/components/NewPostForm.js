2d008b639f394723ae8cfa1704c24fbdbb6fa3e1
ks46 <konastaf46@gmail.com>
2023-08-28 19:44:14 +0300

    textarea in NewPostForm

--- src/pages/Dashboard/components/NewPostForm.js
+++ src/pages/Dashboard/components/NewPostForm.js
@@ -1,6 +1,15 @@
 import '../../../styles/dashboard.css'
 import UserBanner from "../../../components/UserBanner";
-// import InputField from '../../../components/InputField';
+
+function InputFields() {
+
+  return (
+    <textarea
+      className='post-input'
+      placeholder="What's on your mind?"
+    ></textarea>
+  )
+}
 
 function SubmitPostButton() {
   return (
@@ -20,8 +29,7 @@ export default function NewPostForm() {
     <form onSubmit={handleSumbit} className='new-post-form box-container box-container-white'>
       <UserBanner name={name} />
 
-      {/* <InputField type='text' placeholder="What's on your mind?" className='post-input' /> */}
-      <input type='text' className='post-input' placeholder="What's on your mind?" />
+      <InputFields />
 
       <SubmitPostButton />
     </form>
