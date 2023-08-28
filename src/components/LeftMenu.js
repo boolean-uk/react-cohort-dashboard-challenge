bde92501f2c267c1c5ae5024b41fe7eadaf3a77c
ks46 <konastaf46@gmail.com>
2023-08-28 19:15:18 +0300

    made menu items clickable

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,33 +1,43 @@
+import { useState } from 'react'
 import '../styles/left_menu.css'
 import HomeIcon from './HomeIcon'
 import ProfileIcon from './ProfileIcon'
+import { Link } from 'react-router-dom'
 
 const menuItems = {
   home: HomeIcon,
   profile: ProfileIcon
 }
 
-function MenuItem({ item, isActive }) {
+function MenuItem({ item, isActive, setCurrentTab, linkTo }) {
   /** NOTE: using dynamic component name as explained here:
-   * https://stackoverflow.com/questions/29875869/react-jsx-dynamic-component-name */
+   * https://stackoverflow.com/questions/29875869/react-jsx-dynamic-component-name
+   */
   const ItemIcon = menuItems[item]
   const activeClass = isActive ? 'active' : 'inactive'
   const capitalizedItem = item[0].toUpperCase() + item.slice(1)
 
   return (
-    <li className={`menu-item ${activeClass}`}>
-      <ItemIcon isActive={isActive} />
-      <p>{capitalizedItem}</p>
-    </li>
+    <Link to={linkTo}>
+      <li
+        className={`menu-item ${activeClass}`}
+        onClick={() => setCurrentTab(item)}
+      >
+        <ItemIcon isActive={isActive} />
+        <p>{capitalizedItem}</p>
+      </li>
+    </Link>
   )
 }
 
 export default function LeftMenu() {
+  const [currentTab, setCurrentTab] = useState('home')
+
   return (
     <nav className='left-menu'>
       <ul>
-        <MenuItem item={'home'} isActive={true} />
-        <MenuItem item={'profile'} isActive={false} />
+        <MenuItem item={'home'} isActive={currentTab === 'home'} setCurrentTab={setCurrentTab} linkTo='/' />
+        <MenuItem item={'profile'} isActive={currentTab === 'profile'} setCurrentTab={setCurrentTab} linkTo='/profile' />
       </ul>
     </nav>
   )
