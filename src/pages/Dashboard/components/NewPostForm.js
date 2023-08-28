3648c18b495262a9e2dd02337f301d071758e439
ks46 <konastaf46@gmail.com>
2023-08-28 20:17:09 +0300

    NewPostForm functionality

--- src/pages/Dashboard/components/NewPostForm.js
+++ src/pages/Dashboard/components/NewPostForm.js
@@ -1,10 +1,14 @@
 import '../../../styles/dashboard.css'
 import UserBanner from "../../../components/UserBanner";
+import { useContext, useState } from 'react';
+import DataContext from '../../../DataContext';
 
-function InputFields() {
-
+function InputFields({ value, handleChange }) {
   return (
     <textarea
+      name='newPost'
+      value={value}
+      onChange={handleChange}
       className='post-input'
       placeholder="What's on your mind?"
     ></textarea>
@@ -20,17 +24,41 @@ function SubmitPostButton() {
 }
 
 export default function NewPostForm() {
-  const name = 'Alex Walker'
+  const [formData, setFormData] = useState('')
+  const { user } = useContext(DataContext)
+
+  const handleChange = event => setFormData(event.target.value)
+
   const handleSumbit = (event) => {
     event.preventDefault()
+    const [title, body] = formData.split('\n\n')
+    console.log('title:', title)
+    console.log('body:', body)
+    setFormData('')
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: user.id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      ;
   }
 
   return (
-    <form onSubmit={handleSumbit} className='new-post-form box-container box-container-white'>
-      <UserBanner name={name} />
-
-      <InputFields />
-
+    <form
+      onSubmit={handleSumbit}
+      className='new-post-form box-container box-container-white'
+    >
+      <UserBanner name={user.name} />
+      <InputFields value={formData} handleChange={handleChange} />
       <SubmitPostButton />
     </form>
   )
