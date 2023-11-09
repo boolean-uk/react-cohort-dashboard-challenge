48bb3a3c325a48afeef65b79850c62c0259705c7
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-09 20:27:47 +0000

    added props to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import UserIcon from "../UserIcon";
 import WebsiteTitle from "./components/WebsiteTitle";
 
-export default function Header() {
+import { contactProps } from "../../utilities/propTypeDefs";
+
+export default function Header({ user }) {
+  const { firstName, lastName } = user;
   return (
     <header className="app-header bg-cohort-blue flex items-center justify-between px-16">
       <WebsiteTitle />
-      <UserIcon />
+      <UserIcon contact={user} firstName={firstName} lastName={lastName} />
     </header>
   );
 }
+
+Header.propTypes = contactProps;
