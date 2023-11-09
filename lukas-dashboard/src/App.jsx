8c20551b76d2b2ff624a708111478cda503cd787
Lukas Dembicki <lad_777@hotmail.com>
2023-11-09 11:46:56 +0000

    App created, dependencies install, Component folders created, Functional Components made.

---
+++ lukas-dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/header'
+import Main from './components/main'
+import LeftMenu from './components/left-menu'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <LeftMenu/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
