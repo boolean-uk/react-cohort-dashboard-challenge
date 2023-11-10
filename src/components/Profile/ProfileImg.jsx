356e66394e9da4c514269e07ad93cbf792829570
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-10 14:17:34 +0100

    tried outsourcing the fetching users to the usersjs but it didn't work

--- src/components/Profile/ProfileImg.jsx
+++ src/components/Profile/ProfileImg.jsx
@@ -9,17 +9,15 @@ export default function ProfileImg({userId, size}) {
   const styles = { backgroundColor: `hsl(${Math.random()*360}, 60%, 50%)` };
 
   const findUser = () => {
-    console.log(userId)
     const foundUser = users.find(user => user.contactId === userId)
     setUser(foundUser)
-    console.log(user)
   }
 
   useEffect(findUser, [])
 
   return (
     <div className={`profile-img ${size}`} style={styles} >
-      {user.firstName[0] + user.lastName[0]}
+      {user.firstName[0]+user.lastName[0]}
     </div>
   )
 }
