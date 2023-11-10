78fe1001b1aeeebd317b069091fcf45dc08b4c11
Satoki <satokiito@gmail.com>
2023-11-10 18:56:37 +0000

    core - AddCommentForm extracted out into separate component

---
+++ src/components/Main/components/AddCommentForm.jsx
@@ -0,0 +1,23 @@
+import SubmitCommentArrow from '../../../../_assets/submit-comment-arrow.svg'
+
+function AddCommentForm() {
+
+    return (
+        <form className="new-comment grid">
+                <label className="new-comment-label" htmlFor="new-comment">
+                    <input className="new-comment-input"
+                        type="text"
+                        id="new-comment"
+                        name="new-comment"
+                        placeholder="Add a comment..."
+                        // value='new-comment'
+                    />
+                </label>
+                <button className="comment-submit-button" type="submit">
+                    <img className='submit-comment-arrow' src={SubmitCommentArrow} width={30} alt="submit comment arrow" />
+                </button>
+            </form>
+    )
+}
+
+export default AddCommentForm
--- src/components/Main/components/Comments.jsx
+++ src/components/Main/components/Comments.jsx
@@ -1,25 +1,12 @@
 import LoggedInProfileCircle from '../../Shared/LoggedInProfileCircle'
-import submitCommentArrow from '../../../../_assets/submit-comment-arrow.svg'
+import AddCommentForm from './AddCommentForm'
 
 function Comments({ loggedInUserInitials }) {
 
     return (
         <section className="comment grid">
-            <LoggedInProfileCircle loggedInUserInitials={loggedInUserInitials}></LoggedInProfileCircle>
-            <form className="new-comment grid">
-                <label className="new-comment-label" htmlFor="new-comment">
-                    <input className="new-comment-input"
-                        type="text"
-                        id="new-comment"
-                        name="new-comment"
-                        placeholder="Add a comment..."
-                        // value='new-comment'
-                    />
-                </label>
-                <button className="comment-submit-button" type="submit">
-                    <img className='submit-comment-arrow' src={submitCommentArrow} width={30} alt="submit comment arrow" />
-                </button>
-            </form>
+            <LoggedInProfileCircle loggedInUserInitials={loggedInUserInitials} />
+            <AddCommentForm />
         </section>
     )
 }
