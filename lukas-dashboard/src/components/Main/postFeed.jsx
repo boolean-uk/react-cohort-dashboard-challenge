63121e5c86a9894b79fd1d9921fa217c0582de3f
Lukas Dembicki <lad_777@hotmail.com>
2023-11-10 11:50:30 +0000

    Structure being built for main post page

--- lukas-dashboard/src/components/Main/postFeed.jsx
+++ lukas-dashboard/src/components/Main/postFeed.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { get, contactURL, postURL } from "../client.jsx"
+import ProfileLogo from "../Reusable/profileLogo";
 
 
 
@@ -13,14 +14,45 @@ export default function PostFeed() {
             .then(data => setUsers(data))
 
         get(postURL)
-        .then(data=> setPosts(data))
+            .then(data => setPosts(data))
 
-    },[])
-console.log(users)
-console.log(posts)
+    }, [])
+    console.log(users)
+    console.log(posts)
 
     return (
-        <div>HI</div>
-
+        // <div>
+        //     <ProfileLogo />
+        //     <PostName/>
+        //     <Title/>
+        // </div>
+        // <hr />
+        // <div>
+        //     <p>See previous comments</p>
+        //     <Comment/>
+        // </div>
+        <>
+            <div className="postInfo">
+                <ProfileLogo></ProfileLogo>
+                <h2>Lukas Dembicki</h2>
+                <p>This is a Title</p>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, rem nemo quidem animi id quos possimus ipsam maxime magni aspernatur fugit cumque delectus voluptate doloribus natus voluptatibus unde deserunt sed.
+            </p>
+            <div>
+                <hr />
+                <p>See previous comments</p>
+            </div>
+            <div className="comment">
+                <ProfileLogo></ProfileLogo>
+                <h3>Fiona Hutchison</h3>
+                <p>This is a comment</p>
+            </div>
+            <div>
+                <form action="">
+                    <input type="text" placeholder=" Add a comment" />
+                </form>
+            </div>
+        </>
     )
 }
