32ce5178e1a2e08c3276518f8774cad79b860231
aayushlama4008 <aayushlama4008@gmail.com>
2023-11-13 19:29:50 +0100

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { Link, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 
-import "./App.css";
+import { useEffect, useState } from "react";
 
+import "./App.css";
 function App() {
+  const [posts,setPosts] = useState('')
+  const root = 'https://boolean-api-server.fly.dev/aayushlama4008/post'
+
+  const  fetchPost  = () =>{
+      fetch(root)
+      .then((response)=>response.json())
+      .then((data)=> setPosts(data))
+  }
+
+  useEffect((fetchPost),[])
+  console.log(posts)
+
   return (
    <Routes>
     <Route path="/" element={<Home/>}>
