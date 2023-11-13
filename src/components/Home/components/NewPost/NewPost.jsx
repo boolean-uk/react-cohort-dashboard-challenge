5b48d6fd455ff31548b521eff46dc7d1e8134862
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-13 15:30:56 +0000

    pixels make all the difference

--- src/components/Home/components/NewPost/NewPost.jsx
+++ src/components/Home/components/NewPost/NewPost.jsx
@@ -5,7 +5,7 @@ import NewPostForm from "./components/NewPostForm";
 
 export default function NewPost({ setLoadPosts, user }) {
   return (
-    <section className="new-post app-card flex gap-2">
+    <section className="new-post app-card flex gap-4">
       <UserIcon contact={user} />
       <NewPostForm user={user} setLoadPosts={setLoadPosts} />
     </section>
