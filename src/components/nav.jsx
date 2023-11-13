34db53df02a159fa4692cb8cda2edf703b069aa6
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-11-13 12:31:51 +0000

    added links to nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,16 @@
-
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-return(
-  <nav> 
-<img src="src/assets/profile.svg" alt="profile" />
-<img src="src/assets/home.svg" alt="home" />
-  </nav>
-)
-
-}
+  return (
+    <nav>
+      <Link to="/">
+        <img src="src/assets/home.svg" alt="home" />
+      </Link>
+      <Link to="profile">
+        <img src="src/assets/profile.svg" alt="profile" />
+      </Link>
+    </nav>
+  );
+};
 
 export default Nav;
