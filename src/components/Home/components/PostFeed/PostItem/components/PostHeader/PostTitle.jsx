397e49084b40efb72eb1f253a0ab12e42c2a8903
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-14 15:59:11 +0000

    solved big link boi

--- src/components/Home/components/PostFeed/PostItem/components/PostHeader/PostTitle.jsx
+++ src/components/Home/components/PostFeed/PostItem/components/PostHeader/PostTitle.jsx
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function PostTitle({ postId, title }) {
   return (
-    <Link to={`/post/${postId}`}>
-      <h3 className="post-title text-sm font-bold text-cohort-title">
-        {title}
-      </h3>
-    </Link>
+    <h3 className="post-title text-sm font-bold text-cohort-title">
+      <Link to={`/post/${postId}`}>{title}</Link>
+    </h3>
   );
 }
 
