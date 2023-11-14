21e6fa819bb685a07021c88357061241f3b3919f
Satoki <satokiito@gmail.com>
2023-11-14 17:31:43 +0000

    extension - clicking 'see previous comments' expands or collapses the comment list

--- src/components/Main/components/Comment/CommentContent.jsx
+++ src/components/Main/components/Comment/CommentContent.jsx
@@ -6,30 +6,62 @@ function CommentContent({ post, loggedInUser, loggedInUserInitials, URL }) {
 
     const [allComments, setAllComments] = useState(null)
     const [showThreeComments, setShowThreeComments] = useState(null)
+    const [displayComments, setDisplayComments] = useState(null)
+    const [showAll, setShowAll] = useState(false)
 
     // FETCH ALL COMMENTS
     useEffect(() => {
         fetch(`${URL}/post/${post.id}/comment`)
         // fetch(`${URL}/post/${1}/comment`)
             .then(res => res.json())
-            .then(data => setAllComments(data))
+            .then(data => {
+                setAllComments(data)
+                setShowAll(true)
+            })
     }, [URL, post])
 
     // FETCH THREE COMMENTS PER POST
     useEffect(() => {
         fetch(`${URL}/post/${post.id}/comment`)
             .then(res => res.json())
-            .then(data => setShowThreeComments(data.splice(0, 3)))
+            .then(data => {
+                setShowThreeComments(data.splice(0, 3))
+                setShowAll(false)
+            })
     }, [URL, post])
 
-    console.log(showThreeComments)
-    
-    if (!allComments) return <p>Comment not available</p>
+    function handleToggle() {
+        setShowAll(!showAll)
+    }
+
+    useEffect(() => {
+        if (showAll) {
+            setDisplayComments(allComments)
+        }
+        else setDisplayComments(showThreeComments)
+    }, [allComments, showAll, showThreeComments])
+
+
+    if (!allComments || !showThreeComments || !displayComments) return <p>Comment not available</p>
+
+    if (allComments.length > 3) {
+        return (
+            <section className="comment-container grid">
+            <ul className='post-comments-list grid'>
+                <h5 id='x' onClick={() => handleToggle()}>Show previous comments</h5>
+                {displayComments.map((comment) =>
+                    <PostComments key={comment.id} comment={comment} URL={URL} />
+                )}
+            </ul>
+            <AddNewComment post={post} loggedInUser={loggedInUser} loggedInUserInitials={loggedInUserInitials} URL={URL}setAllComments={setAllComments} />
+        </section>
+        )
+    }
 
     return (
         <section className="comment-container grid">
             <ul className='post-comments-list grid'>
-                {showThreeComments.map((comment) => 
+                {showThreeComments.map((comment) =>
                     <PostComments key={comment.id} comment={comment} URL={URL} />
                 )}
             </ul>
