bcefd6f6b73da9abe57b54fc39325a218348a5c9
Elizabeth <lizbeepmc@gmail.com>
2023-11-15 12:39:51 +0300

    Done with arranging fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,40 @@ const App = () => {
   const [showNames, setShowNames] = useState([]);
   const [showComments, setShowComments] = useState([]);
   const fetchData = () => {
-    const URL1 = "https://boolean-api-server.fly.dev/Elizabethcodes44/contact"
-    const URL2 = "https://boolean-api-server.fly.dev/Elizabethcodes44/post"
-    //const URL3 = `https://boolean-api-server.fly.dev/Elizabethcodes44/${post.id}/comment`
-    const getUrl1 = axios.get(URL1)
-    const getUrl2 = axios.get(URL2)
+    const URL1 = "https://boolean-api-server.fly.dev/Elizabethcodes44/contact";
+    const URL2 = "https://boolean-api-server.fly.dev/Elizabethcodes44/post";
+    //const URL3 = https://boolean-api-server.fly.dev/Elizabethcodes44/${post.id}/comment
+    const getUrl1 = axios.get(URL1);
+    const getUrl2 = axios.get(URL2);
     axios.all([getUrl1, getUrl2]).then(
-      axios.spread((...allData)=> {
-        const allDataNames= allData[0].data
-        const allDataContent= allData[1].data
+      axios.spread((...allData) => {
+        const allDataNames = allData[0].data;
+        const allDataContent = allData[1].data;
         setShowNames(allData[0].data);
-        setShowComments(allData[1].data)
-        
+        setShowComments(allData[1].data);
+
         console.log(allDataNames);
         console.log(allDataContent);
-        console.log("AllData [0]",allData[0].data) 
-        
-
+        console.log("AllData [0]", allData[0].data);
       })
-    )
+    );
   };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
+  console.log("these are the names:", showNames);
+  console.log("these are the comments:", showComments);
 
   return (
     <>
       <div className="app-Container">
         <Header></Header>
-        <Home className="main-Container">
-          {showNames}
-          {showComments}
-         
-        </Home>
+        <Home
+          className="main-Container"
+          showNames={showNames}
+          showComments={showComments}
+        ></Home>
         <LeftMenu className="LeftMenu"></LeftMenu>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -54,5 +54,4 @@ const App = () => {
     </>
   );
 };
-
 export default App;
--- src/home.jsx
+++ src/home.jsx
@@ -1,21 +1,38 @@
 
-function Home() {
-  return (
-    <>
-      <main>
-        <label>
-          <input
-            type="text"
-            name="user"
-            placeholder="what's on your mind?"
-          ></input>
-          
-        </label>
-        <input className="form__submit" type="submit" value="Post" />
-    
 
-      </main>
-    </>
+const Home = ({ showNames, showComments }) => {
+  // Check if showNames and showComments are arrays before mapping
+  if (!Array.isArray(showNames) || !Array.isArray(showComments)) {
+    return <p></p>;
+  }
+
+  // Determine the minimum length of the two arrays
+  const minLength = Math.min(showNames.length, showComments.length);
+
+  return (
+    <div>
+      {/* Iterate up to the minimum length of the two arrays */}
+      {Array.from({ length: minLength }, (_, index) => (
+        <div key={index}>
+          {/* Display first name and last name */}
+          {index < showNames.length && (
+            <>
+              <p>{`${showNames[index].firstName}`}</p>
+              <p>{`${showNames[index].lastName}`}</p>
+            </>
+          )}
+
+          {/* Display title and content */}
+          {index < showComments.length && (
+            <>
+              <p>{`${showComments[index].title}`}</p>
+              <p>{`${showComments[index].content}`}</p>
+            </>
+          )}
+        </div>
+      ))}
+    </div>
   );
-}
+};
+
 export default Home;
