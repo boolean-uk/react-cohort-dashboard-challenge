89bd93cda41dd19a4cbd6f073652134fde786950
ilham saleh <ilham2saleh@gmail.com>
2023-11-16 09:35:09 +0000

    posts are sorted in descending order

--- src/Components/Posts/index.jsx
+++ src/Components/Posts/index.jsx
@@ -9,11 +9,13 @@ export default function Posts(props) {
 
     // const limitedPosts = posts.slice(0, 3)
 
+    const sortedPosts = posts.sort((a, b) => b.id - a.id)
+
     return (
         <div className="main">
              <PostForm getPosts={getPosts} post={post}/>
             <div className="posts-container">
-                {posts.map((post, index) => (
+                {sortedPosts.map((post, index) => (
                         <ul key={index}>
                             <Post post={post} getComments={getComments} comments={comments}/>
                         </ul>
