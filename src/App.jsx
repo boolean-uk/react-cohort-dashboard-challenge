fc84ddd5d6bd66361d78f5432081cc12acc5a5db
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-11-26 13:00:55 +0100

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./views/Home";
 import NavBar from "./components/NavBar/NavBar";
+
 import PostPageSection from "./views/Home/components/PostPageSection/PostPageSection";
 
 import "./App.css";
