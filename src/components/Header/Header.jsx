52ee579d585d0c5db5388a5f7becf6e895cf607c
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-28 15:51:02 +0100

    Add navigation to root

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import titleHeader from "../../assets/title-header.svg";
 import "@styles/Header.css";
 import ProfileCircle from "../ProfileCircle";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <nav className="header">
       <div className="header-group">
@@ -10,6 +12,7 @@ export default function Header() {
           className="header-logo"
           src={titleHeader}
           alt="cohort-manager-logo"
+          onClick={() => navigate("/")}
         />
         <ProfileCircle
           className="profile-circle"
