2fcaeb1d81ac18664ef5fffb6e6edd84506a44b9
Linus Willmont <linus.willmont@se.experis.com>
2024-03-05 09:27:39 +0100

    Planning&Prep

---
+++ src/utilites/ColorScheme.js
@@ -0,0 +1,13 @@
+export const colorTheme = {
+	main: "#000046",
+	formBackground: "#e6ebf5",
+	buttonHover: "#64dc78",
+	postTitle: "#64648c",
+	bodyBackground: "#F0F5FA",
+};
+
+export const svg = {
+	headerIcon: "./_assets/title-header-svg.md",
+	homeIcon: "./_assets/home-icon-svg.md",
+	profileIcon: "./_assets/profile-icon-svg.md",
+};
