57076011b86552c87710bfe2c76b500ba305394d
llllllll-l <vincent.werelius@gmail.com>
2024-03-05 10:06:24 +0100

    sidebar and css (from own fun proj)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import Header from "./components/Header.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+
+      <aside>
+        <div className="sidebar">
+          <a href="#" data-target="dashboard">
+            <span className="material-symbols-outlined">home</span>
+            <h3>Home</h3>
+          </a>
+          <a href="#" data-target="users">
+            <span className="material-symbols-outlined">account_circle</span>
+            <h3>Profile</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">logout</span>
+            <h3>Logout</h3>
+          </a>
+        </div>
+      </aside>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import TitleHeader from "../assets/title_header_svg.svg";
+
+function Header() {
+  return (
+    <>
+      <section>
+        <img src={TitleHeader} alt="Home Icon" />
+      </section>
+    </>
+  );
+}
+
+export default Header;
