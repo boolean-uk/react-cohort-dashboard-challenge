3f6f095adc4258570dec0a49aaf9104f07ffae5e
Matthias Greeven <matsgreeven@gmail.com>
2024-03-06 10:07:24 +0100

    Finished Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState, useEffect, createContext } from 'react'
 import './App.css'
+import Dashboard from './Dashboard'
+import Header from './Header'
+import LeftMenu from './LeftMenu'
+const URL = "https://boolean-api-server.fly.dev/thegrevling/contact"
+export const UsersContext = createContext()
+export const CurrentUserContext = createContext()
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [currentUser,setCurrentUser]=useState(undefined)
+  
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(setUsers)
+  }, [])
+
+  useEffect(()=>{
+    setCurrentUser(users.find((user)=>user.id===1))
+  },[users])
 
   return (
-    <div></div>
+    <div>
+      <CurrentUserContext.Provider value={{currentUser:currentUser, setCurrentUser:setCurrentUser}} >
+        <Header/>
+        <LeftMenu />
+        <UsersContext.Provider value={users}>
+          <Dashboard />
+        </UsersContext.Provider>
+      </CurrentUserContext.Provider>
+    </div>
   )
 }
 
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import UserProfile from './Pages/UserProfile/UserProfile'
+import PostView from './Pages/Home/PostView/PostView'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post/:id' element={<PostView />} />
+        <Route path='/profile/:id' element={<UserProfile />} />
+      </Routes>
+    </div>
   )
 }
 
