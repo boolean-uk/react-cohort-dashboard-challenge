7b0ffcf2084af3aa01c36f278a8fdb4c3b02d25e
Maha Riazi <maha.riazi@hotmail.no>
2024-03-06 15:46:17 +0100

    comment om posts implemented

--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -1,52 +1,86 @@
-  import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 
 function PostListItem({ post }) {
-    const [comments, setComments] = useState([])
+  const [comments, setComments] = useState([]);
+  const initCommentInput = {
+    contactId: 10,
+    postId: post ? post.id : null,
+    content: "",
+  };
+  const [commentInput, setCommentInput] = useState(initCommentInput);
 
-    useEffect(() => {
-      fetch(
-        `https://boolean-api-server.fly.dev/maha897/post/${post.id}/comment`
-      )
-        .then((response) => response.json())
-        .then(setComments)
-    })
+  useEffect(() => {
+    fetch(`https://boolean-api-server.fly.dev/maha897/post/${post.id}/comment`)
+      .then((response) => response.json())
+      .then(setComments);
+  }, [post.id]);
 
-    return (
-      <li className="post-li">
-        <div className="post-header">
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch(
+      `https://boolean-api-server.fly.dev/maha897/post/${post.id}/comment`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(commentInput),
+      }
+    )
+      .then((response) => response.json())
+      .then((newComment) => {
+        setComments([...comments, newComment]);
+        setCommentInput(initCommentInput);
+      });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCommentInput({ ...commentInput, [name]: value });
+  }
+
+  return (
+    <li className="post-li">
+      <div className="post-header">
+        {/* TODO user info */}
+        <h3>{post.title}</h3>
+      </div>
+      <p>{post.content}</p>
+
+      <div className="comments">
+        <div className="posted-comments">
+          <h3>Comments: </h3>
+          <ul className="comments-ul">
+            {comments.map((comment) => (
+              <li key={comment.id}>
+                User #{comment.contactId}: {comment.content}
+                <br /> <br />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <form className="comment-form" onSubmit={handleSubmit}>
           {/* TODO user info */}
-          <h3>{post.title}</h3>
-        </div>
-        <p>{post.content}</p>
-
-        <div className="comments">
-          <div className="posted-comments">
-            <h3>Comments: </h3>
-            <ul className="comments-ul">
-              {comments.map((comment) => (
-                <li key={comment.id}>
-                  User #{comment.contactId}: {comment.content}
-                  <br /> <br />
-                </li> 
-              ))}
-            </ul>
+          <div className="embed-submit-field">
+            <input 
+              name="content" 
+              placeholder="Add a comment..." 
+              onChange={handleChange}
+              value={commentInput.content}
+            />
+            <button type="submit">Comment</button>
           </div>
-
-          <form className="comment-form" onSubmit={() => {}}>
-            {/* TODO user info */}
-            <div className="embed-submit-field">
-              <input name="comment-input" placeholder="Add a comment..." />
-              <button type="submit">Comment</button>
-            </div>
-          </form>
-        </div>
-      </li>
-    );
+        </form>
+      </div>
+    </li>
+  );
 }
 
 PostListItem.propTypes = {
-    post: PropTypes.object
-}
+  post: PropTypes.object,
+};
 
-export default PostListItem
+export default PostListItem;
