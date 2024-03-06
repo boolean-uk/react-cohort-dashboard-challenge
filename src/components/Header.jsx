c83a980175766603696d4692fbd333a7bbaaa63b
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-03-06 09:25:39 +0100

    initial style layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import title_header_svg from '../assets/title-header-svg.svg'
+import profile_icon from '../assets/profile-icon.svg'
 import '../styles/header.css'
 
 const Header = () => {
   return (
-    <header>
-      <img src={title_header_svg}/>
-      {/* <img src={profile_icon}/> */}
+    <header className='header'>
+      <img className='title-header' src={title_header_svg}/>
+      <img src={profile_icon}/>
       
     </header>
   );
--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import profile_icon from '../assets/profile-icon.svg'
+import home_icon from '../assets/home-icon.svg'
+import '../styles/leftNav.css'
+
 
 const LeftNav = () => {
   return (
     <div className='nav-container'>
-      
+      <div>
+        <img src={home_icon}/>
+        <p>Home</p>
+      </div>
+      <div>
+      <img src={profile_icon}/>
+      <p>Profile</p>
+      </div>
     </div>
   );
 }
