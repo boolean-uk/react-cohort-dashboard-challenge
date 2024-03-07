4c2f8608feba0602866b2f32533e9ce2409999c6
Matthias Greeven <matsgreeven@gmail.com>
2024-03-07 08:11:04 +0100

    Added Comment Expantion extention

--- src/Pages/Home/Components/CommentList.jsx
+++ src/Pages/Home/Components/CommentList.jsx
@@ -3,35 +3,45 @@ import CreateComment from './CreateComment'
 import CommentItem from './CommentItem'
 import '../Home.css'
 
-function CommentList({postId}) {
-    const URL = `https://boolean-api-server.fly.dev/thegrevling/post/${postId}/comment`
-    const [comments, setComments] = useState([])
+function CommentList({ postId }) {
+  const URL = `https://boolean-api-server.fly.dev/thegrevling/post/${postId}/comment`
+  const [comments, setComments] = useState([])
+  const [showAllComments, setShowAllComments] = useState(false);
 
-    
-    const fetchComments = async () => {
-        try {
-          const response = await fetch(URL);
-          if (response.ok) {
-            const data = await response.json();
-            setComments(data);
-          } else {
-            console.error('Failed to fetch posts');
-          }
-        } catch (error) {
-          console.error('Error fetching posts:', error);
-        }
-      };
 
-      useEffect(() => {
-        fetchComments();
-    }, [])
+  const fetchComments = async () => {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+        setComments(data);
+      } else {
+        console.error('Failed to fetch posts');
+      }
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
 
-    return (
-        <div className="comment-list-container">
-            {comments && comments.map((comment, index) => <CommentItem comment={comment} key={index} />)}
-            <CreateComment fetchComments={fetchComments} postId={postId}/>
+  useEffect(() => {
+    fetchComments();
+  }, [])
+
+  const displayedComments = showAllComments ? comments : comments.slice(0, 3);
+
+  return (
+    <div className="comment-list-container">
+      {displayedComments.map((comment, index) => (
+        <CommentItem comment={comment} key={index} />
+      ))}
+      {comments.length > 3 && !showAllComments && (
+        <div className="see-more-comments" onClick={() => setShowAllComments(true)}>
+          See previous comments
         </div>
-    )
+      )}
+      <CreateComment fetchComments={fetchComments} postId={postId} />
+    </div>
+  );
 }
 
 export default CommentList
