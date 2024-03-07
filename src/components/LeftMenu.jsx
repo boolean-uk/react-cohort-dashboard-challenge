6b83aafe48cdb1ea88dd7519701d3e86f48df333
Linus Willmont <linus.willmont@se.experis.com>
2024-03-07 12:03:06 +0100

    Change to NavLink in leftMenu

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,31 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import homeIcon from "../assets/home-icon.svg";
 import profileIcon from "../assets/profile-icon.svg";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { LoggedInUserContext } from "../App";
 
 export const LeftMenu = () => {
-	const [active, setActive] = useState("home");
 	const user = useContext(LoggedInUserContext);
 
 	return (
 		<menu className="menu">
-			<Link
-				to={`/posts`}
-				className={active === "home" ? "active" : ""}
-				onClick={() => setActive("home")}
-			>
+			<NavLink to={"/posts"} end activeClassName="active">
 				<img src={homeIcon} alt="Home" style={{}} />
 				<p>Home</p>
-			</Link>
-			<Link
-				to={`/profile/${user.id}`}
-				className={active === "profile" ? "active" : ""}
-				onClick={() => setActive("profile")}
-			>
+			</NavLink>
+			<NavLink to={`/profile/${user.id}`} activeClassName="active">
 				<img src={profileIcon} alt="Profile" />
 				<p>Profile</p>
-			</Link>
+			</NavLink>
 		</menu>
 	);
 };
