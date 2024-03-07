5104530e9c04db091f0919053e066d12e5199c94
Mathias Fredriksen <mathias.fredriksen@no.experis.com>
2024-03-07 10:22:02 +0100

    Base layout done. Missing page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import './App.css'
+import Header from './pages/Header';
+import SideMenu from './pages/SideMenu';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Profile from './pages/Profile';
+
+export const DataContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activePage, setActivePage] = useState("/")
 
   return (
-    <div>
-      <h1>New React Application</h1>
+    <div className='view'>
+      <Header/>
+      <div className="view-container">
+        <DataContext.Provider value={{activePage: activePage}}>
+          <SideMenu />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </DataContext.Provider>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+      <main className="content">
+        <h2>Main Content Area</h2>
+        {/* Add your page content here */}
+        <p>This is the main content area.</p>
+      </main>
+    );
+}
+
+export default Home
