b51cdea418b840c24a69e8cc5676e1133a986dce
Philip Morud <philip.morud@no.experis.com>
2024-03-08 11:11:52 +0100

    added commenting component

--- src/components/Dashboard/Content/Posts/Post/CreateComment.jsx
+++ src/components/Dashboard/Content/Posts/Post/CreateComment.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserIconComponent from '../../../../assets/user-icon'
+import { loginUserContext } from '../../../../../App'
+import './CreateComment.css'
 
-export default function CreateCommentComponent({postId}) {
+export default function CreateCommentComponent({ postId }) {
+    const { loginUser } = useContext(loginUserContext)
+
     return (
-        <div>CreateCommentComponent
-
+        <div className='createcomment'>
+            <UserIconComponent classname='icon' user={loginUser.get}/>
+            <input className='comment' placeholder='Comment'/>
+            <button className='button'>Comment</button>
         </div>
     )
 }
