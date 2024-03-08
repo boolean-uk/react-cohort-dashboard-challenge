315f72b1984db3b3475a7738b91d14ec8eafe903
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-03-08 11:46:40 +0100

    The active route is higlighted on the left menu

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -2,25 +2,25 @@
 import '../styles/LeftMenu.css';
 
 import homeIcon from '../assets/home-icon.svg';
-import profileIcon from '../assets/profile-icon.svg'
-import { Link } from 'react-router-dom';
+import profileIcon from '../assets/profile-icon.svg';
+import { NavLink } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './App';
 
 export const LeftMenu = () => {
-    const authUser = useContext(AuthContext)
+    const authUser = useContext(AuthContext);
 
     return (
-      <div className="left-menu">
-        <Link to="/" className="options">
-          <img className="icon" src={homeIcon} alt="home-icon" />
-          <p>Home</p>
-        </Link>
+        <div className="left-menu">
+            <NavLink to="/" className="options" >
+                <img className="icon" src={homeIcon} alt="home-icon" />
+                <p>Home</p>
+            </NavLink>
 
-        <Link to={`/profile/${authUser.id}`} className="options">
-          <img className="icon" src={profileIcon} alt="profile-icon" />
-          <p>Profile</p>
-        </Link>
-      </div>
+            <NavLink to={`/profile/${authUser.id}`} className="options" >
+                <img className="icon" src={profileIcon} alt="profile-icon" />
+                <p>Profile</p>
+            </NavLink>
+        </div>
     );
 }
