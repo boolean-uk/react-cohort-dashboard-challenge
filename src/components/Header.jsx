91ca14807258b1d977e193344483a48580395769
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-03-08 15:14:59 +0100

    Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import '../style/header.css';
 import titleHeader from '../assets/title-header.svg';
 import { UserContextAPIContext } from '../contextAPI/UserContextAPI';
+import { Link } from 'react-router-dom';
 
 function Header() {
 
@@ -14,11 +15,15 @@ function Header() {
 
   return (
     <div className='header'>
+      
       <img src={titleHeader} className='header-img'/>
 
-      <div className="circle header-profile" style={{backgroundColor: user.favouriteColour}}>
-        <span className="initials">{user.firstName[0]}{user.lastName[0]}</span>
-      </div>
+      <Link to={"/profile"} style={{textDecoration : "none"}}>
+        <div className="circle header-profile" style={{backgroundColor: user.favouriteColour}}>
+          <span className="initials">{user.firstName[0]}{user.lastName[0]}</span>
+        </div>
+      </Link>
+
     </div>
   )
 }
