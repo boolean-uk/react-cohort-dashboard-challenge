cbaf2159bbf133861e768c863bc79599bcfc6729
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-08 10:04:49 +0100

    Bug - Resolved a context scope issue in PostView

--- src/Components/ContentView/ContentView.jsx
+++ src/Components/ContentView/ContentView.jsx
@@ -3,9 +3,28 @@ import { Routes, Route } from 'react-router-dom'
 import PostView from "./PostView/PostView"
 import PostDetails from "./PostDetails/PostDetails"
 import ProfileView from "./ProfileView/ProfileView"
+import { useState, useEffect } from 'react'
+import { basePostUrl } from "@/Utils/apiUtils"
+import { PostsContext } from "@/Utils/contexts"
 
 const ContentView = () => {  
+    const [posts, setPosts] = useState([])
+
+    const fetchPosts = async () => {
+        await fetch(basePostUrl)
+            .then((res) => res.json())
+            .then((res) => res.reverse()) // New posts will appear first
+            .then((res) => setPosts([...res]))
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    },[])
+
     return (
+        <PostsContext.Provider
+            value={{posts: posts, setPosts: setPosts, fetchPosts: fetchPosts}}
+        >
         <div className="content-view-container scroll-container">
             <Routes>
                 <Route 
@@ -22,6 +41,7 @@ const ContentView = () => {
                 />
             </Routes>
         </div>
+        </PostsContext.Provider>
     )
 }
 
--- src/Components/ContentView/PostView/PostView.jsx
+++ src/Components/ContentView/PostView/PostView.jsx
@@ -1,32 +1,13 @@
-import { useState, useEffect } from 'react'
-import { basePostUrl } from "@/Utils/apiUtils"
-import { PostsContext } from "@/Utils/contexts"
+
 import PostList from "./PostList/PostList"
 import CreatePost from "./CreatePost/CreatePost"
 
 const PostView = () => {
-    const [posts, setPosts] = useState([])
-
-    const fetchPosts = async () => {
-        await fetch(basePostUrl)
-            .then((res) => res.json())
-            .then((res) => res.reverse()) // New posts will appear first
-            .then((res) => setPosts([...res]))
-    }
-
-    useEffect(() => {
-        fetchPosts()
-    },[])
-
     return (
-        <PostsContext.Provider
-            value={{posts: posts, setPosts: setPosts, fetchPosts: fetchPosts}}
-        >
         <div>
             <CreatePost />
             <PostList />
         </div>
-        </PostsContext.Provider>
     )
 }
 
