074d67cbb0e746b775bb16afa91ae669bc55819c
Maha Riazi <maha.riazi@hotmail.no>
2024-03-08 12:58:32 +0100

    profile page almost pretty

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -6,23 +6,27 @@ function ProfilePage() {
   const { state } = useLocation();
   return (
     <div className="profile-page">
-      <Avatar
-        name={`${state.user.firstName} ${state.user.lastName}`}
-        round={true}
-      />
-      <h2>
-        {state.user.firstName} {state.user.lastName}
-      </h2>
-
-      {Object.entries(state.user).map(([name, value]) =>
-        name !== "id" &&
-        name !== "favouriteColour" &&
-        name !== "profileImage" ? (
-          <p key={name}>
-            <b>{name}:</b> {value}
-          </p>
-        ) : null
-      )}
+      <div className="profile-details-container">
+        <Avatar
+          name={`${state.user.firstName} ${state.user.lastName}`}
+          round={true}
+        />
+        <h2>
+          {state.user.firstName} {state.user.lastName}
+        </h2>
+        <div className="line-container">
+          <hr className="line"></hr>
+        </div>
+        {Object.entries(state.user).map(([name, value]) =>
+          name !== "id" &&
+          name !== "favouriteColour" &&
+          name !== "profileImage" ? (
+            <p key={name}>
+              <b>{name}:</b> {value}
+            </p>
+          ) : null
+        )}
+      </div>
     </div>
   );
 }
