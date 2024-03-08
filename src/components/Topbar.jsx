e16804d8be6ddc0ed417f9f724e8d5dea2331c7c
Philip Morud <philip.morud@no.experis.com>
2024-03-08 09:38:09 +0100

    added profile icon to topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Topbar.css'
+import UserIconComponent from './assets/user-icon'
+import { loginUserContext } from '../App';
 
 export default function TopbarComponent() {
+
+    const { loginUser } = useContext(loginUserContext)
+
     return (
         <div className='topbar' >
-            <svg
-                className='logo'
+            <svg className='logo'
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 356 56"
@@ -19,7 +23,9 @@ export default function TopbarComponent() {
                     d="M36.068 22.934H13.07c-.618 0-1.122.505-1.122 1.122v1.122c0 .617.504 1.122 1.121 1.122h23c.616 0 1.121-.505 1.121-1.122v-1.122c0-.617-.505-1.122-1.122-1.122ZM19.38 19.568h10.378c.617 0 1.121-.505 1.121-1.122v-1.122c0-.617-.505-1.121-1.122-1.121H19.38c-.617 0-1.122.505-1.122 1.122v1.121c0 .617.505 1.122 1.122 1.122Zm10.378 10.097H19.38c-.617 0-1.122.505-1.122 1.122v1.122c0 .617.505 1.122 1.122 1.122h10.378c.617 0 1.121-.505 1.121-1.122v-1.122c0-.617-.505-1.122-1.122-1.122Z"
                 />
             </svg>
-
+            <div className='usericon'>
+                <UserIconComponent user={loginUser.get}/>
+            </div>
         </div>
     )
 }
