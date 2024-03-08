5fce622b25662608a9cc6b8b5f6d4d259a77e4f0
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-03-08 09:36:49 +0100

    added styled-components and styled layout of post section

--- src/components/PostComponent.jsx
+++ src/components/PostComponent.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import CreatePost from './CreatePost';
 import PostList from './PostList';
+import styled from 'styled-components';
 
+const ComponentContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  gap: 15px;
+`
 const PostComponent = () => {
   return (
-    <div>
+    <ComponentContainer>
       <CreatePost/>
       <PostList/>
-    </div>
+    </ComponentContainer>
   );
 }
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,16 +1,24 @@
 import React, { useContext } from 'react';
 import { PostContext } from '../App';
 import Post from './Post'
+import styled from 'styled-components';
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`
+
 
 const PostList = () => {
   const {posts = [], setPosts} = useContext(PostContext)
 
   return (
-    <div className='list-container'>
+    <ListContainer>
       {posts.toReversed().map((post, index) => {
         return <Post post={post} key={index}/>
       })}
-    </div>
+    </ListContainer>
   );
 }
 
