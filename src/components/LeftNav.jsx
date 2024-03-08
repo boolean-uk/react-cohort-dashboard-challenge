981bfbb434c7c2c1c1eec4c56b55f840183708ab
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-03-08 10:26:11 +0100

    added styling for LeftNav

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 import profile_icon from '../assets/profile-icon.svg'
 import home_icon from '../assets/home-icon.svg'
 import '../styles/leftNav.css'
-import PostComponent from './PostComponent';
-import Profile from './Profile';
-
 
 const LeftNav = () => {
+  const nav = useNavigate()
+  const [selected, setSelected] = useState("/home")
+
+  const handleRouteClick = (route) => {
+    setSelected(route)
+    nav(route)
+  }
   return (
     <div className='nav-container'>
-      <div className='nav-element'>
-        <img src={home_icon}/>
-        <Link to={'/home'} >Home</Link>
+      <div className='nav-element' 
+          onClick={() => handleRouteClick("/home")} 
+        style={{ background: selected === "/home" ? '#e6ebf5' : '#ffffff'}}>
+        <img src={home_icon}/>  
+        <p to={'/home'}>Home</p>
       </div>
-      <div className='nav-element'>
+      <div className='nav-element' 
+          onClick={() => handleRouteClick("/profile/1")} 
+          style={{ background: selected === "/profile/1" ? '#e6ebf5' : '#ffffff' }}>
       <img src={profile_icon}/>
-        <Link to={'/profile/1'}>Profile</Link>
+        <p>Profile</p>
       </div>
     </div>
   );
