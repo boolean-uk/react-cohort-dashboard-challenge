603da83ef6a8e88eda680e32aa503feb6256950c
Mahmoud Ali <mahmoodali2965.ma@gmail.com>
2024-03-10 17:25:33 +0100

    Edit & Save profile info fixed

--- src/components/post/profile/Form.jsx
+++ src/components/post/profile/Form.jsx
@@ -1,19 +1,54 @@
 
 import PropTypes from "prop-types";
-
 import { useEffect, useState } from "react";
 import ProfileSubForm from "./ProfileSubForm";
+import { BaseURL } from "../../../App";
+
+
+
 
 const Form = ({ owner }) => {
     const submitProfile = async (event) => {
         event.preventDefault();
-    };
+        try {
+            const response = await fetch(BaseURL + /contact/ + owner.id, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
 
+            if (!response.ok) {
+                throw new Error('Failed to save data');
+            }
+
+            console.log('Data saved successfully');
+            setEditMode(false);
+        } catch (error) {
+            console.error('Error saving data:', error.message);
+        }
+    };
+   
     const [userData, setUserData] = useState(owner);
 
     useEffect(() => {
         setUserData(owner);
     }, [owner]);
+   
+
+    const [editMode, setEditMode] = useState(false);
+    
+
+    useEffect(() => {
+        setUserData(owner);
+    }, [owner]);
+
+    const toggleEditMode = () => {
+        setEditMode(!editMode);
+    };
+   
+
     const subforms = [
         {
             title: "Account info",
@@ -42,12 +77,19 @@ const Form = ({ owner }) => {
                         subform={subform}
                         userData={userData}
                         setUserData={setUserData}
+                        editMode={editMode}
                     />
                 ))}
             </div>
+            
             <div className="button-container">
-                <button type="submit">Save</button>
-            </div>
+                {!editMode && (
+                    <button type="button" onClick={toggleEditMode}>
+                        Edit
+                    </button>
+                )}
+                {editMode && <button type="submit">Save</button>}
+            </div> 
         </form>
     );
 };
