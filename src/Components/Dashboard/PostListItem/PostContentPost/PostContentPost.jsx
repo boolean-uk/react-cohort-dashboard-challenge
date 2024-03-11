1fce7b866a5cd735afd9f8f488318394933bb6e8
kristianverduin <kristianverduin@gmail.com>
2024-03-11 08:58:26 +0100

    Added post deletion

--- src/Components/Dashboard/PostListItem/PostContentPost/PostContentPost.jsx
+++ src/Components/Dashboard/PostListItem/PostContentPost/PostContentPost.jsx
@@ -2,8 +2,29 @@ import ProfilePicture from '../../../ProfilePicture/ProfilePicture'
 import { Link } from 'react-router-dom'
 import PropTypes from "prop-types"
 import "./PostContentPost.css"
+import { useContext } from 'react'
+import { LoadingContext, PostContext } from '../../Dashboard'
 
 function PostContentPost( { contact, post } ) {
+  const { posts, setPosts } = useContext(PostContext)
+  const { setLoading } = useContext(LoadingContext)
+  
+  async function handleDelete() {
+
+    const deleteRequest = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(post)
+    }
+    setLoading(true)
+    await fetch("https://boolean-api-server.fly.dev/kristianverduin/post/" + post.id, deleteRequest)
+      .then(res => res.json())
+      .then(setPosts(posts.filter(p => p.id !== post.id)))
+    setLoading(false)
+  }
+
   return (
     <div className="post-content-post">
       <div className="post-header">
@@ -13,6 +34,7 @@ function PostContentPost( { contact, post } ) {
           <Link to={`/view/${post.id}`}><h4>{post.title}</h4></Link>
         </div>
       </div>
+      <button onClick={handleDelete}>Delete</button>
       <p className='post-content-text'>{post.content}</p>
     </div>
   )
