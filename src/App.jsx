2250682eb235b7478d69193b4b846ce103dfdcfa
Giar Rehani <giarreh1@gmail.com>
2024-04-03 11:55:30 +0200

    Replaced BrowserRouter with HashRouter to prepare for production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header'
 import Posts from './components/posts/Posts'
