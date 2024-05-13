2076d8ece25b7659bc41ca481688a2c1defb276b
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-13 11:29:43 +0200

    add header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+import ProfileImage from "./ProfileImage";
+
+export default function Header() {
+    return (
+        <header>
+            <Logo />
+            <ProfileImage />
+        </header>
+    )
+}
---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,7 @@
+export default function ProfileImage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
