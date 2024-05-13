4bed8b1bc1df1c5f8c0277e46e39b7550992338e
Angus Townsley <angustownsley@gmail.com>
2024-05-13 10:52:42 +0100

    Add inital component structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header/Index'
+import NavBar from './components/NavBar'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <NavBar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
