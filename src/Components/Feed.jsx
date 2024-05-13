1460eb1b21031008e78ea9bb2b97e894b7b8e28d
Will Baxter <bax.will@googlemail.com>
2024-05-13 18:40:25 +0100

    Feed renders with all posts and attributed users

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,10 +1,37 @@
 import NewPost from "../Subcomponents/NewPost"
 import PostCard from "../Subcomponents/PostCard"
+import { useState, useEffect } from "react"
+
 export default function Feed () {
+    const [posts, setPosts] = useState([])
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        getUsers()
+        getPosts()
+    }, [])
+
+    const getPosts = async () => {
+        const data = await fetch('https://boolean-api-server.fly.dev/MrStashy/post')
+        const json = await data.json()
+        setPosts(json)
+    }
+
+    const getUsers = async () => {
+        const data = await fetch('https://boolean-api-server.fly.dev/MrStashy/contact')
+        const json = await data.json()
+        setUsers(json)
+    }
+
     return (
         <div className="m-5 flex flex-col gap-3">
         <NewPost />
-        <PostCard />
+        {posts.map((post, index) => {
+            return(
+                <PostCard post={post} users={users} key={index}/>
+            )
+        })}
+        
         </div>
     )
 }
--- src/Subcomponents/PostCard.jsx
+++ src/Subcomponents/PostCard.jsx
@@ -1,18 +1,22 @@
 import InitialIcon from "./InitialIcon";
 
-export default function PostCard() {
+
+export default function PostCard({ post, users }) {
+ 
+     let currentUser = users.find((user) => {return user.id === post.contactId})
+     
   return (
     <article className="text-cohortBlue h-auto gap-2 p-3 rounded-md bg-white">
       <header className="flex place-items-center gap-2 m-2">
-        <InitialIcon />
+        <InitialIcon user={currentUser}/>
         <div className="heading">
-          <p>Name name</p>
-          <p>Title title title</p>
+          <p><strong>{currentUser.firstName} {currentUser.lastName}</strong></p>
+          <p>{post.title}</p>
         </div>
       </header>
-      <p className="ml-2">Post body post body</p>
+      <p className="ml-2">{post.content}</p>
       <hr className="h-px bg-inputGrey mx-2 border-0"/>
-      <article className="flex gap-2 p-3 rounded-md bg-white">
+      <article className="flex gap-2 p-2 rounded-md bg-white">
             <InitialIcon />
             <input className='bg-inputGrey p-2 flex-1 rounded-md' name='newPost' placeholder="Add a comment..."></input>
         </article>
