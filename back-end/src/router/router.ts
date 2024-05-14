9ad44de89cf52f543c756d801fda6f8b225b9ce4
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-14 22:07:49 +0100

    add signup route with database connection

--- back-end/src/router/router.ts
+++ back-end/src/router/router.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import { loginHandler, validateLoginParams } from "./endpoints/login";
+import { signUpHandler, validateSignUpParams } from "./endpoints/signup";
 const router = express.Router();
 
 router.post("/login", validateLoginParams, loginHandler);
+router.post("/signup", validateSignUpParams, signUpHandler);
 
 export default router;
