c6d4d2bf96d7960c62d263d008bcf4f6cb05afd9
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-14 20:54:26 +0100

    add test login verificationwith database

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
+const uri = process.env.DB_URI as string;
+const DB_NAME = process.env.DB_NAME as string;
 
 import app from "./server";
+import DatabaseClient from "./database/DatabaseClient";
 
 const port = process.env.PORT || 3000;
 
+export const mongoClient = new DatabaseClient(uri, DB_NAME);
+//
+mongoClient.connect();
+process.on("SIGINT", mongoClient.close);
+process.on("SIGTERM", mongoClient.close);
+//
 app.listen(port, () => {
 	console.log(`[server]: server is running on port ${port}`);
 });
--- back-end/src/router/endpoints/login.ts
+++ back-end/src/router/endpoints/login.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { NextFunction } from "express-serve-static-core";
-import { validateParams } from "../validators/params.validator";
-import { log } from "console";
+import { mongoClient } from "../..";
 import { validateEmail, validatePassword } from "../validators/form.validator";
+import { validateParams } from "../validators/params.validator";
 import { ValidatorCallback } from "../validators/validator.type";
 
 type LOGIN_PARAMS = {
@@ -10,8 +10,19 @@ type LOGIN_PARAMS = {
 	password: string;
 };
 
-export function loginHandler(req: Request, res: Response, next: NextFunction) {
+export async function loginHandler(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
 	//validate login
+	const { email, password }: { email: string; password: string } = req.body;
+
+	for await (const user of mongoClient.find("users", { email })) {
+		if (user.password === password)
+			return res.status(200).json({ token: "123", user: req.body.name });
+		else return res.status(401).json({ message: "Invalid Password" });
+	}
 
 	//get user data
 	//send token + data
