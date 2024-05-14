508ed879227afde3c33e063e38784bd7007a1da2
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-14 11:23:05 +0100

    add server router and default 500 err response

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,13 +1,25 @@
 import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import router from "./router";
 //
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+//
 app.get("/", (_req: Request, res: Response) => {
 	res.status(200).json({ message: "API server is running" });
 });
+app.use(router);
+
+// Default Err
+app.use(
+	(_err: Error, _req: Request, res: Response, _next: NextFunction): void => {
+		res.status(500).json({
+			message: "An unknown error occurred.",
+		});
+	}
+);
 
 export default app;
