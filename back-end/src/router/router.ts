76d8090cc57fcd1f63ad7fdde5f00a0a6a0c8035
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-14 23:35:17 +0100

    fix naming mistake and remove unused dependencies

--- back-end/src/router/router.ts
+++ back-end/src/router/router.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express";
-import validateLoginParamsMiddleware from "./endpoints/login/signup.validator.middleware";
-import validateSignUpParamsMiddleware from "./endpoints/signup/signup.validator.middleware";
+import express from "express";
 import loginMiddleware from "./endpoints/login/login.middleware";
+import validateLoginParamsMiddleware from "./endpoints/login/login.validator.middleware";
 import signUpMiddleware from "./endpoints/signup/signup.middleware";
+import validateSignUpParamsMiddleware from "./endpoints/signup/signup.validator.middleware";
 
 const router = express.Router();
 
