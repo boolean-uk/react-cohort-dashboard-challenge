15d3ef92af2afb76d8efaf217f33a996f9830027
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-16 19:36:49 +0100

    create edit posts endpoint

---
+++ back-end/src/router/endpoints/feed/editPost.middleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { dbClient } from "../../..";
+import { DB_COLLECTIONS } from "../../../database/collections.enum";
+import { POSTS_FILTERS_SCHEMA } from "../common/models/postsFilters.schema";
+import { POST_SCHEMA } from "../../../database/models/post.schema";
+import { JwtPayload } from "jsonwebtoken";
+
+export default async function editPostMiddleware(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const postData = req.body.data as POST_SCHEMA;
+	try {
+		//NOTE: This try catch is probably not doing anything since the only function that can throw is insertPost...and it already has a trycatch
+		//Set the timestamp to "now"
+		postData.timestamp = new Date().toJSON();
+
+		const { status, ...db_data } = await dbClient.updatePost(
+			{ _id: new ObjectId(postData._id) },
+			postData
+		);
+		return res.status(status).json(db_data);
+	} catch (error) {
+		return res.status(500).json({
+			message: "Internal server error while updating post",
+			error,
+		});
+	}
+}
