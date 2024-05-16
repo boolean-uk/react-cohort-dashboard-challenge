46d5b3c181b8eb1424a3cc162339cab1c1ab218e
homonoviscoding <mama.leye@gmail.com>
2024-05-16 12:55:33 +0100

    third commit finished styling first landing page

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -11,22 +11,14 @@ export default function posts() {
                 <li className="list-posts">
                     <div className="post-publisher">
                         <div className="user">SF</div>
-                        <div><p>firstName lastName</p><p>title</p></div>
+                        <div><p>firstName lastName</p><p className="post-title">title</p></div>
                     </div>
                     <div className="post-content">paragraph</div>
                     <div className="comment-section">
                         <p className="logged-user">ML</p>
-                        <div>
-                            <input id="post-input" type="text" value='' placeholder="Add a comment..." />
-                            <button className="post-btn">
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#e8eaed"
-                                >
-                                    <path 
-                                        d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"
-                                    />
-                                </svg>
-                            </button>
+                        <div className="comment-input">
+                            <input id="input-comment" type="text" name='comment' value='' placeholder="Add a comment..." />
+                            <button className="material-symbols-outlined">send</button>    
                         </div>
                     </div>
                 </li>
