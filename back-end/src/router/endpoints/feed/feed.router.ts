45e2b182a56e074450c33d41932ac6ee82ce8f5d
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-16 19:38:00 +0100

    connect new endpoints to main router

--- back-end/src/router/endpoints/feed/feed.router.ts
+++ back-end/src/router/endpoints/feed/feed.router.ts
@@ -4,11 +4,15 @@ import getFeedPostsMiddleware from "./getFeedPosts.middleware";
 import validatePostFiltersMiddleware from "../common/validators/posts/postsFilters.validator.middleware";
 import validatePostDataMiddleware from "../common/validators/posts/postData.validator.middleware";
 import postNewPostMiddleware from "./postNewPost.middleware";
+import editPostMiddleware from "./editPost.middleware";
+import deletePostMiddleware from "./deletePost.middleware";
 
 const router = express.Router();
 router.use(authenticateCookieMiddleware);
 
-router.get("/",  getFeedPostsMiddleware);
+router.get("/", getFeedPostsMiddleware);
 router.post("/", validatePostDataMiddleware, postNewPostMiddleware);
+router.put("/", validatePostDataMiddleware, editPostMiddleware);
+router.delete("/", validatePostDataMiddleware, deletePostMiddleware);
 
 export default router;
