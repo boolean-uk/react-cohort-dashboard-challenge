b75a390e2a82f9f6b51cd58e87f11fcbaaef4c04
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-16 19:16:18 +0100

    add support for loggout and minor fixes

--- back-end/src/router/auth/auth.crypto.ts
+++ back-end/src/router/auth/auth.crypto.ts
@@ -20,8 +20,11 @@ function generateAccessToken(payload: any, expire: string) {
 }
 
 function verifyAccessToken(token: string) {
+	//NOTE: This error is meant to break the server!
+	// the env variable only loads at server start...so...if its undefined, something is wrong with server env settings
 	if (!process.env.JTW_ACCESS_CODE)
 		throw new Error("Can't verify token without secret code");
+
 	try {
 		const res = jwt.verify(token, process.env.JTW_ACCESS_CODE);
 
@@ -32,7 +35,7 @@ function verifyAccessToken(token: string) {
 		//LOOKUP: https://stackoverflow.com/a/36698015/22510505
 		//return 422 when token "is valid" but was tampered
 
-		return { message: "Invalid Token", status: 400 };
+		return { error, message: "Invalid Token", status: 400 };
 	}
 }
 
--- back-end/src/router/auth/authenticateCookie.middleware.ts
+++ back-end/src/router/auth/authenticateCookie.middleware.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import auth from "./auth.crypto";
 import { JwtPayload } from "jsonwebtoken";
 import { log } from "console";
+import { dbClient } from "../..";
+import { DB_COLLECTIONS } from "../../database/collections.enum";
 
 export default async function authenticateCookieMiddleware(
 	req: Request,
@@ -14,6 +16,16 @@ export default async function authenticateCookieMiddleware(
 		return res
 			.status(401)
 			.json({ message: "Missing or expired access token" });
+	}
+	//== Cookie didn't expired, but user already logout out and cookie is on blacklist
+	const isCookieBlacklisted = await dbClient.has(
+		DB_COLLECTIONS.BLACKLISTED_COOKIES,
+		{ cookie }
+	);
+	if (isCookieBlacklisted) {
+		return res
+			.status(401)
+			.json({ message: "Your session is invalid. Please login again" });
 	}
 
 	const {
