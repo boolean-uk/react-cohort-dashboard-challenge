8b6287f77792752a0d7c6fbc2ca54e921cfe2a2f
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-16 22:07:17 +0100

    remove redundant code from logoutMiddleware, and apply the authenticateCookie before the logout

--- back-end/src/router/endpoints/logout/logout.middleware.ts
+++ back-end/src/router/endpoints/logout/logout.middleware.ts
@@ -1,28 +1,14 @@
 import { Request, Response } from "express";
 import { NextFunction } from "express-serve-static-core";
 import { dbClient } from "../../..";
-import auth from "../../auth/auth.crypto";
 
 export default async function logoutMiddleware(
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) {
-	const cookie = req.headers?.cookie?.split("=")[1];
-	//== Cookie expired
-	if (!cookie) {
-		return res.status(401).json({ message: "You were not logged in" });
-	}
-	const {
-		status: verifyStatus,
-		message: verifyMessage,
-		payload,
-	} = auth.verifyAccessToken(cookie as string);
+	const cookie = req.headers?.cookie?.split("=")[1] as string;
 
-	//== Cookie is invalid (was hacked?)
-	if (verifyStatus !== 200) {
-		return res.status(verifyStatus).json({ message: verifyMessage });
-	}
 	//== Cookie is valid -> add to blacklist
 	const { status, ...db_response } = await dbClient.insertBlacklistCookie(
 		cookie
--- back-end/src/router/main.router.ts
+++ back-end/src/router/main.router.ts
@@ -5,16 +5,17 @@ import signUpMiddleware from "./endpoints/signup/signup.middleware";
 import validateSignUpParamsMiddleware from "./endpoints/signup/signup.validator.middleware";
 import userMiddleware from "./endpoints/:user/:user.middleware";
 import userRouter from "./endpoints/:user/:user.router";
-import feedRouter from "./endpoints/feed/feed.router";
+import feedRouter from "./endpoints/posts/posts.router";
 import logoutMiddleware from "./endpoints/logout/logout.middleware";
+import authenticateCookieMiddleware from "./auth/authenticateCookie.middleware";
 
 const router = express.Router();
 
 router.post("/signup", validateSignUpParamsMiddleware, signUpMiddleware);
 router.post("/login", validateLoginParamsMiddleware, loginMiddleware);
-router.get("/logout", logoutMiddleware);
-router.use("/feed", feedRouter);
-//dont forget to load this as last!
+router.get("/logout", authenticateCookieMiddleware, logoutMiddleware);
+router.use("/posts", feedRouter);
+//don't forget to load this as last!
 router.use("/:id", userRouter);
 
 export default router;
