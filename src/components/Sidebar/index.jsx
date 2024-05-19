5cd39b8242530c1b3248f052076ef689221fc73d
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-05-19 11:03:33 +0100

    5. sidebar basic structure

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,25 @@
 
-function Sidebar(){
-    return <>Sidebar</>
-}
+import { Link } from "react-router-dom"
+import iconHome from "./assets/home-icon.svg"
+import iconProfile from "./assets/profile-icon.svg"
+import "./Sidebar.css"
+export default function Sidebar(){
 
-export default Sidebar
+
+    return (
+        <>
+            <nav>
+                <Link to="/">
+                    <div className="nav-item">
+                        <img className="nav-home" src={iconHome} alt="home" />
+                    </div>
+                </Link>
+                <Link to="/profile">
+                    <div className="nav-item">
+                        <img className="nav-profile" src={iconProfile} alt="profile" />
+                    </div>
+                </Link>
+            </nav>
+        </>
+    )
+}
