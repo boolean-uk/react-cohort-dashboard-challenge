f4ac81730a83d7602488c67a8aba923bbaca76e7
Periklis <perik76@gmail.com>
2024-05-20 20:09:18 +0300

    Better styleing header and sidebar now stay in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import { Routes, Route } from "react-router-dom"
-
 import Header from "./components/Header"
 import SideBar from "./components/SideBar"
 import Main from "./components/Main"
 import UserProfilePage from "./components/UserProfilePage"
 import { UsersProvider } from "./context/UsersContext"
 import { PostsProvider } from "./context/PostsContext"
-// import NewPost from "./components/NewPost"
 
 function App() {
 	return (
-		<>
-			<PostsProvider>
-				<UsersProvider>
-					<header className=''>
+		<PostsProvider>
+			<UsersProvider>
+				<div className='h-screen flex flex-col'>
+					<header className='w-full'>
 						<Header />
 					</header>
-					<div className='grid grid-cols-[180px,_1fr] '>
-						<SideBar />
-						<main className=''>
+					<div className='flex flex-grow overflow-hidden'>
+						<div className='w-[180px] flex-shrink-0'>
+							<SideBar />
+						</div>
+						<main className='flex-grow overflow-auto'>
 							<Routes>
 								<Route path='/' element={<Main />} />
 								<Route
 									path='/user/:id'
 									element={<UserProfilePage />}
 								/>
-								{/* <Route path='/newPost' element={<NewPost />} /> */}
 							</Routes>
 						</main>
 					</div>
-				</UsersProvider>
-			</PostsProvider>
-		</>
+				</div>
+			</UsersProvider>
+		</PostsProvider>
 	)
 }
 
